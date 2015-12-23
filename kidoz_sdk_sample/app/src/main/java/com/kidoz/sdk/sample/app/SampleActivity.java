5681780cd81ab3bf559652fda0e5fb14033dcd7c
EugineP <evgeny@kidoz.net>
2015-12-23 17:00:13 +0200

    Update using SDK v 0.3.0
    Added Banner unit sample

--- kidoz_sdk_sample/app/src/main/java/com/kidoz/sdk/sample/app/SampleActivity.java
+++ kidoz_sdk_sample/app/src/main/java/com/kidoz/sdk/sample/app/SampleActivity.java
@@ -4,14 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-
 import com.kidoz.sdk.api.FeedButton;
 import com.kidoz.sdk.api.FeedView;
+import com.kidoz.sdk.api.KidozBanner;
 import com.kidoz.sdk.api.PanelView;
 import com.kidoz.sdk.api.interfaces.IOnFeedViewEventListener;
 import com.kidoz.sdk.api.interfaces.IOnPanelViewEventListener;
-import com.kidoz.sdk.api.ui_views.panel_view.HANDLE_POSITION;
-import com.kidoz.sdk.api.ui_views.panel_view.PANEL_TYPE;
+import com.kidoz.sdk.api.ui_views.kidoz_banner.KidozBannerListener;
 
 /**
  * Created by KIDOZ.
@@ -29,6 +28,11 @@ public class SampleActivity extends Activity {
     private PanelView mPanelView;
 
     /**
+     * Kidoz Panel instance
+     */
+    private KidozBanner mKidozBanner;
+
+    /**
      * Kidoz Family compliant Panel instance
      */
    // private FamilyPanelView mFamilyPanelView;
@@ -37,58 +41,107 @@ public class SampleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /** Set main view layout containing Kidoz Feed View Button */
+        /** Set main view layout containing*/
         setContentView(R.layout.activity_sample);
 
-        /** Get reference to Kidoz Feed View button */
+
+        /** Initiate Button view */
+        initFeedButton();
+
+        /** Initiate Panel view */
+        initFeedPanel();
+
+        /** Initiate Banner view */
+        initBannerView();
+    }
+
+
+
+    /** Initiate Button view */
+    private void initFeedButton() {
+        /** Get reference to KIDOZ Feed View button */
         mFeedButton = (FeedButton) findViewById(R.id.kidozBtn_view);
 
-        /** To add view events listeners do the following...
-         *  Also FeedView object can be used to open and close the FeedView view manually
+        /**
+         * To add view events listeners do the following... Also FeedView object
+         * can be used to open and close the FeedView view manually
          * */
         FeedView feedView = mFeedButton.getFeedView();
         feedView.setOnFeedViewEventListener(new IOnFeedViewEventListener() {
             @Override
             public void onDismissView() {
 
-                /**  View has been dismissed by user or action */
-                Toast.makeText(SampleActivity.this, "Feed View is dismissed", Toast.LENGTH_SHORT).show();
+                /** View has been dismissed by user or action */
+                Toast.makeText(SampleActivity.this, "Feed View is dismissed",
+                        Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onReadyToShow() {
 
-                /** Event is launched moment before the view is opened,
-                 *  This allows the developer to stop some process currently running or make any additional actions...
+                /**
+                 * Event is launched moment before the view is opened, This
+                 * allows the developer to stop some process currently running
+                 * or make any additional actions...
                  * */
-                Toast.makeText(SampleActivity.this, "Feed View is ready to be shown..", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SampleActivity.this,
+                        "Feed View is ready to be shown..", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onViewReady() {
+                // TODO Auto-generated method stub
+                Toast.makeText(SampleActivity.this, "Feed View Ready",
+                        Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-
-        /** Get reference to Kidoz Panel View */
+    /** Initiate Panel view */
+    private void initFeedPanel() {
+        /** Get reference to KIDOZ Panel View */
         mPanelView = (PanelView) findViewById(R.id.kidozPanel_view);
 
-        /** To add view events listeners do the following...
+        /**
+         * To add view events listeners do the following...
          * */
         mPanelView.setOnPanelViewEventListener(new IOnPanelViewEventListener() {
             @Override
             public void onPanelViewCollapsed() {
-
+                /** Panel View Collapsed by user or action */
+                Toast.makeText(SampleActivity.this, "PanelView Collapsed",
+                        Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onPanelViewExpanded() {
-
+                /** Panel View Expanded by user or action */
+                Toast.makeText(SampleActivity.this, "PanelView Expanded",
+                        Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onPanelReady() {
-                /** Function invoked when the panel is finished initiation and have beed drawn */
+                /** Panel View Ready action */
+                Toast.makeText(SampleActivity.this, "PanelView View Ready",
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
         /** Set panel configuration to a different layout style */
-        mPanelView.setPanelConfiguration(PANEL_TYPE.BOTTOM, HANDLE_POSITION.END);
+        // mPanelView.setPanelConfiguration(PANEL_TYPE.RIGHT,HANDLE_POSITION.START);
+    }
+
+    /** Initiate Banner view */
+    private void initBannerView() {
+        mKidozBanner = (KidozBanner) findViewById(R.id.kidozBanner_view);
+        mKidozBanner.setKidozBannerListener(new KidozBannerListener() {
+            @Override
+            public void onBannerReady() {
+                super.onBannerReady();
+                mKidozBanner.showBanner();
+            }
+        });
     }
 }
