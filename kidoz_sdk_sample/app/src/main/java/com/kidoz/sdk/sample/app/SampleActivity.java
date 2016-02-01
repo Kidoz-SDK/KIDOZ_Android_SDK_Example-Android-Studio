30f7edfa9da442d76fd2244b797a0b02239d33de
EugineP <evgeny@kidoz.net>
2016-02-01 16:52:57 +0200

    Update to version 0.4.0
    New version of the SDK0.4.0 + lost of bug fixes and new widget support ,
    Flexi Point View.

--- kidoz_sdk_sample/app/src/main/java/com/kidoz/sdk/sample/app/SampleActivity.java
+++ kidoz_sdk_sample/app/src/main/java/com/kidoz/sdk/sample/app/SampleActivity.java
@@ -6,10 +6,13 @@ import android.widget.Toast;
 
 import com.kidoz.sdk.api.FeedButton;
 import com.kidoz.sdk.api.FeedView;
+import com.kidoz.sdk.api.FlexiView;
 import com.kidoz.sdk.api.KidozBanner;
 import com.kidoz.sdk.api.PanelView;
+import com.kidoz.sdk.api.interfaces.FlexiViewListener;
 import com.kidoz.sdk.api.interfaces.IOnFeedViewEventListener;
 import com.kidoz.sdk.api.interfaces.IOnPanelViewEventListener;
+import com.kidoz.sdk.api.ui_views.flexi_view.FLEXI_POSITION;
 import com.kidoz.sdk.api.ui_views.kidoz_banner.KidozBannerListener;
 
 /**
@@ -33,17 +36,16 @@ public class SampleActivity extends Activity {
     private KidozBanner mKidozBanner;
 
     /**
-     * Kidoz Family compliant Panel instance
+     * Flexi View instance
      */
-   // private FamilyPanelView mFamilyPanelView;
+    private FlexiView mFlexiView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /** Set main view layout containing*/
+        /** Set main view layout containing Kidoz Feed View Button */
         setContentView(R.layout.activity_sample);
-
 
         /** Initiate Button view */
         initFeedButton();
@@ -53,11 +55,14 @@ public class SampleActivity extends Activity {
 
         /** Initiate Banner view */
         initBannerView();
+
+        /** Flexi Point view */
+        initFlexiView();
     }
 
-
-
-    /** Initiate Button view */
+    /**
+     * Initiate Button view
+     */
     private void initFeedButton() {
         /** Get reference to KIDOZ Feed View button */
         mFeedButton = (FeedButton) findViewById(R.id.kidozBtn_view);
@@ -98,7 +103,9 @@ public class SampleActivity extends Activity {
         });
     }
 
-    /** Initiate Panel view */
+    /**
+     * Initiate Panel view
+     */
     private void initFeedPanel() {
         /** Get reference to KIDOZ Panel View */
         mPanelView = (PanelView) findViewById(R.id.kidozPanel_view);
@@ -133,7 +140,9 @@ public class SampleActivity extends Activity {
         // mPanelView.setPanelConfiguration(PANEL_TYPE.RIGHT,HANDLE_POSITION.START);
     }
 
-    /** Initiate Banner view */
+    /**
+     * Initiate Banner view
+     */
     private void initBannerView() {
         mKidozBanner = (KidozBanner) findViewById(R.id.kidozBanner_view);
         mKidozBanner.setKidozBannerListener(new KidozBannerListener() {
@@ -141,6 +150,37 @@ public class SampleActivity extends Activity {
             public void onBannerReady() {
                 super.onBannerReady();
                 mKidozBanner.showBanner();
+            }
+        });
+    }
+
+    /**
+     * Initiate Flexi Point view
+     */
+    private void initFlexiView() {
+        mFlexiView = (FlexiView) findViewById(R.id.kidozFlexi_view);
+        mFlexiView.setAutoShow(true);
+        mFlexiView.setFlexiViewInitialPosition(FLEXI_POSITION.TOP_START);
+        mFlexiView.setOnFlexiViewEventListener(new FlexiViewListener() {
+            @Override
+            public void onViewReady() {
+                super.onViewReady();
+                Toast.makeText(SampleActivity.this, "Flexi Ready",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onViewHidden() {
+                super.onViewHidden();
+                Toast.makeText(SampleActivity.this, "Flexi Hidden",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onViewVisible() {
+                super.onViewVisible();
+                Toast.makeText(SampleActivity.this, "Flexi Visible",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
