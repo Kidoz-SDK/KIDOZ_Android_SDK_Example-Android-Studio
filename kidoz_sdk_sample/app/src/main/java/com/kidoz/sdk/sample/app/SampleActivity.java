00b04140050ee240a20b19c9c33fa546e4deb42e
EugineP <evgeny@kidoz.net>
2016-05-03 14:07:09 +0300

    SDK 0.5.4 update
    Sample Updated to use SDK v0.5.4

--- kidoz_sdk_sample/app/src/main/java/com/kidoz/sdk/sample/app/SampleActivity.java
+++ kidoz_sdk_sample/app/src/main/java/com/kidoz/sdk/sample/app/SampleActivity.java
@@ -2,12 +2,14 @@ package com.kidoz.sdk.sample.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.kidoz.sdk.api.FeedButton;
 import com.kidoz.sdk.api.FeedView;
 import com.kidoz.sdk.api.FlexiView;
 import com.kidoz.sdk.api.KidozBanner;
+import com.kidoz.sdk.api.KidozInterstitial;
 import com.kidoz.sdk.api.PanelView;
 import com.kidoz.sdk.api.interfaces.FlexiViewListener;
 import com.kidoz.sdk.api.interfaces.IOnFeedViewEventListener;
@@ -19,8 +21,8 @@ import com.kidoz.sdk.api.ui_views.kidoz_banner.KidozBannerListener;
 /**
  * Created by KIDOZ.
  */
-public class SampleActivity extends Activity {
-
+public class SampleActivity extends Activity
+{
     /**
      * Kidoz Button instance
      */
@@ -41,8 +43,14 @@ public class SampleActivity extends Activity {
      */
     private FlexiView mFlexiView;
 
+    /**
+     * Kidoz interstitial instance
+     */
+    private KidozInterstitial mKidozInterstitial;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         /** Set main view layout containing Kidoz Feed View Button */
@@ -59,12 +67,26 @@ public class SampleActivity extends Activity {
 
         /** Flexi Point view */
         initFlexiView();
+
+        /** Interstitial Sample */
+        initInterstitial();
+
+        findViewById(R.id.interstitialBtn).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mKidozInterstitial.show();
+                Toast.makeText(SampleActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /**
      * Initiate Button view
      */
-    private void initFeedButton() {
+    private void initFeedButton()
+    {
         /** Get reference to KIDOZ Feed View button */
         mFeedButton = (FeedButton) findViewById(R.id.kidozBtn_view);
 
@@ -73,9 +95,11 @@ public class SampleActivity extends Activity {
          * can be used to open and close the FeedView view manually
          * */
         FeedView feedView = mFeedButton.getFeedView();
-        feedView.setOnFeedViewEventListener(new IOnFeedViewEventListener() {
+        feedView.setOnFeedViewEventListener(new IOnFeedViewEventListener()
+        {
             @Override
-            public void onDismissView() {
+            public void onDismissView()
+            {
 
                 /** View has been dismissed by user or action */
                 Toast.makeText(SampleActivity.this, "Feed View is dismissed",
@@ -83,7 +107,8 @@ public class SampleActivity extends Activity {
             }
 
             @Override
-            public void onReadyToShow() {
+            public void onReadyToShow()
+            {
 
                 /**
                  * Event is launched moment before the view is opened, This
@@ -96,64 +121,73 @@ public class SampleActivity extends Activity {
             }
 
             @Override
-            public void onViewReady() {
+            public void onViewReady()
+            {
                 // TODO Auto-generated method stub
                 Toast.makeText(SampleActivity.this, "Feed View Ready",
                         Toast.LENGTH_SHORT).show();
             }
         });
 
-
         /**
-         * Add view listener for general events of player opened/closed
-         * can be added to each Widget type (View) separately
+         * Add view listener for general events of player opene/closed
+         * can be added to each Widget type (View) separetly
          * */
-       feedView.setKidozPlayerListener(new KidozPlayerListener() {
-           @Override
-           public void onPlayerOpen() {
-               super.onPlayerOpen();
+        feedView.setKidozPlayerListener(new KidozPlayerListener()
+        {
+            @Override
+            public void onPlayerOpen()
+            {
+                super.onPlayerOpen();
 
-               Toast.makeText(SampleActivity.this, "Player Opened",
-                       Toast.LENGTH_SHORT).show();
-           }
+                Toast.makeText(SampleActivity.this, "Player Opened",
+                        Toast.LENGTH_SHORT).show();
+            }
 
-           @Override
-           public void onPlayerClose() {
-               super.onPlayerClose();
+            @Override
+            public void onPlayerClose()
+            {
+                super.onPlayerClose();
 
-               Toast.makeText(SampleActivity.this, "Player Closed",
-                       Toast.LENGTH_SHORT).show();
-           }
-       });
+                Toast.makeText(SampleActivity.this, "Player Closed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /**
      * Initiate Panel view
      */
-    private void initFeedPanel() {
+    private void initFeedPanel()
+    {
         /** Get reference to KIDOZ Panel View */
         mPanelView = (PanelView) findViewById(R.id.kidozPanel_view);
+        //mPanelView.setPanelConfiguration(PANEL_TYPE.TOP, HANDLE_POSITION.START);
 
         /**
          * To add view events listeners do the following...
          * */
-        mPanelView.setOnPanelViewEventListener(new IOnPanelViewEventListener() {
+        mPanelView.setOnPanelViewEventListener(new IOnPanelViewEventListener()
+        {
             @Override
-            public void onPanelViewCollapsed() {
+            public void onPanelViewCollapsed()
+            {
                 /** Panel View Collapsed by user or action */
                 Toast.makeText(SampleActivity.this, "PanelView Collapsed",
                         Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onPanelViewExpanded() {
+            public void onPanelViewExpanded()
+            {
                 /** Panel View Expanded by user or action */
                 Toast.makeText(SampleActivity.this, "PanelView Expanded",
                         Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onPanelReady() {
+            public void onPanelReady()
+            {
                 /** Panel View Ready action */
                 Toast.makeText(SampleActivity.this, "PanelView View Ready",
                         Toast.LENGTH_SHORT).show();
@@ -161,17 +195,20 @@ public class SampleActivity extends Activity {
         });
 
         /** Set panel configuration to a different layout style */
-        // mPanelView.setPanelConfiguration(PANEL_TYPE.RIGHT,HANDLE_POSITION.START);
+
     }
 
     /**
      * Initiate Banner view
      */
-    private void initBannerView() {
+    private void initBannerView()
+    {
         mKidozBanner = (KidozBanner) findViewById(R.id.kidozBanner_view);
-        mKidozBanner.setKidozBannerListener(new KidozBannerListener() {
+        mKidozBanner.setKidozBannerListener(new KidozBannerListener()
+        {
             @Override
-            public void onBannerReady() {
+            public void onBannerReady()
+            {
                 super.onBannerReady();
                 mKidozBanner.showBanner();
             }
@@ -181,31 +218,44 @@ public class SampleActivity extends Activity {
     /**
      * Initiate Flexi Point view
      */
-    private void initFlexiView() {
+    private void initFlexiView()
+    {
         mFlexiView = (FlexiView) findViewById(R.id.kidozFlexi_view);
         mFlexiView.setAutoShow(true);
         mFlexiView.setFlexiViewInitialPosition(FLEXI_POSITION.TOP_START);
-        mFlexiView.setOnFlexiViewEventListener(new FlexiViewListener() {
+        mFlexiView.setOnFlexiViewEventListener(new FlexiViewListener()
+        {
             @Override
-            public void onViewReady() {
+            public void onViewReady()
+            {
                 super.onViewReady();
                 Toast.makeText(SampleActivity.this, "Flexi Ready",
                         Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onViewHidden() {
+            public void onViewHidden()
+            {
                 super.onViewHidden();
                 Toast.makeText(SampleActivity.this, "Flexi Hidden",
                         Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onViewVisible() {
+            public void onViewVisible()
+            {
                 super.onViewVisible();
                 Toast.makeText(SampleActivity.this, "Flexi Visible",
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    /**
+     * Initiate Interstitial
+     */
+    private void initInterstitial()
+    {
+        mKidozInterstitial = new KidozInterstitial(this);
     }
 }
